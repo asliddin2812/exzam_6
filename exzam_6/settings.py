054e6f4a6e172ea98e59225fad192c2b0ff7abea
Asliddin Mamatov <169055620+asliddin2812@users.noreply.github.com>
2025-05-10 20:06:50 +0500

    Update settings.py

--- exzam_6/settings.py
+++ exzam_6/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!2^o=#!7x7nj8h)ux)&75b+!rttl*+&3k)61$-q5(a&ayxuk!t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
